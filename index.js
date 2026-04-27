0382baff31af01fd9c7e636b59fed7f87620b45f
Abigail Tandiwe Simelane <tandisimelane24@gmail.com>
2026-04-27 13:23:59 +0300

    Final changes

--- index.js
+++ index.js
@@ -43,5 +43,25 @@ const bookStore = {
     ]
 }
 
-// Write your code here!
+const bookStoreTitle = document.getElementById('header');
+bookStoreTitle.textContent = bookStore.name;
 
+const bookList = document.getElementById('book-list');
+
+bookStore.books.forEach(function(book) {
+    const bookContainer = document.createElement('li');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const bookImage = document.createElement('img');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    bookList.appendChild(bookContainer);
+    console.log('Adding book:', book.title);
+});
